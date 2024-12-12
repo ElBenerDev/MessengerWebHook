8f4056204fecd79ea00dd4230d729d3ed6831b19
bernardoraos90 <bernardoraos90@gmail.com>
2024-12-12 12:41:07 -0500

    changePort

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 const pythonServiceUrl = process.env.PYTHON_SERVICE_URL || 'http://localhost:5000';
 
 app.use(bodyParser.json());
